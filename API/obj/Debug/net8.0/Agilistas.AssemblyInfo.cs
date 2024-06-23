ea76d343e605832d22b9f4bd7084d003778e3ceb
julie <julielerman@gmail.com>
2024-06-23 16:44:51 -0400

    working through it

--- API/obj/Debug/net8.0/Agilistas.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/Agilistas.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Agilistas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9d5be77ed4f6376015474e8b133c6077288d017")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be8528a8a2e361129fb87636177575de91a24c2f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Agilistas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Agilistas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
